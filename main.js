58e4066a2b2689125c7dab09d1f5640e7753f7bf
theroboticcheese <65261941+theroboticcheese@users.noreply.github.com>
2021-06-04 13:36:09 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-var link1 = document.getElementById('link');
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav_section');
+    const overlay = document.querySelector('.overlay');
+    const body = document.querySelector('body');
 
-link1.style.width = "500px";
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav_active');
+        overlay.classList.toggle('overlay_active');
+        body.classList.toggle('body_hidden');
+    });
+
+    nav.addEventListener('click', () => {
+        nav.classList.toggle('nav_active');
+        overlay.classList.toggle('overlay_active');
+        body.classList.toggle('body_hidden');
+    });
+
+
+
+}
+
+navSlide();
